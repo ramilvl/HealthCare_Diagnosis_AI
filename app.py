e76b4423d2df5770760797e11d6514c7e9dc5ab5
Ramil Valiyev <ramil4360616@gmail.com>
2023-12-11 04:00:46 +0400

    research materials are added

--- app.py
+++ app.py
@@ -5,8 +5,34 @@ from pgmpy.inference import VariableElimination
 import pandas as pd
 import numpy as np
 
+
+# How the algorithm works:
+# *The structure of the Bayesian Network is defined using the BayesianNetwork class from the pgmpy.models module.
+# *The network structure specifies how different symptoms are probabilistically related to each other.
+# **The synthetic dataset is generated, and then the Bayesian Network is trained (fitted) using the Maximum Likelihood Estimation method. 
+# **This is done with the fit method of the Bayesian Network model, and the MaximumLikelihoodEstimator is used as the estimator.
+# ***Variable Elimination (VariableElimination class from pgmpy.inference) is set up for performing inference in the Bayesian Network.
+# ***This will be used later to query the network and make predictions based on observed evidence.
+
+# More Information of Used Algorithm:
+# The key components involved in Bayesian Network implementation include defining the network structure, generating a dataset for training, 
+# and then fitting the model to the data.
+#  Afterward, the trained model is used for inference, and in this specific project, it's employed for diagnosing illnesses
+#  based on observed symptoms.
+
+# Library REFERENCE: https://pgmpy.org/param_estimator/mle.html
+# REFERENCE: https://flask.palletsprojects.com/en/3.0.x/
+# https://github.com/JaavLex/shootingclubpy
+# https://realpython.com/tutorials/flask/
+
+# REFERENCE: https://numpy.org/doc/stable/
+
 app = Flask(__name__)
 
+# IMPORTANT RESEARCH PAPER: https://www.cis.upenn.edu/~mkearns/papers/barbados/heckerman.pdf
+# Note: We have learned how to create synthetic_dataset by looking through Page_36 code example structure
+
+# ***EDGES
 model = BayesianNetwork([('Flu', 'Fever'), ('Flu', 'Fatigue'), ('Fever', 'Cough'), ('Fatigue', 'Cough'),
                          ('Flu', 'Headache'), ('Flu', 'SoreThroat'), ('Fever', 'BodyAche'), ('Fatigue', 'BodyAche'),
                          ('Cough', 'ChestPain'), ('BodyAche', 'ChestPain'), ('Flu', 'Chills'), ('Flu', 'Nausea'),
@@ -14,10 +40,34 @@ model = BayesianNetwork([('Flu', 'Fever'), ('Flu', 'Fatigue'), ('Fever', 'Cough'
 
 np.random.seed(42)
 num_samples = 1000
+
+# General Information:
+'''
+A Bayesian Network is a probabilistic graphical model that represents probabilistic relationships among a set of variables.
+In this code, the Bayesian Network is represented using the pgmpy library, 
+where nodes correspond to symptoms, and edges represent probabilistic dependencies between them.
+'''
+
+'''
+In Case of Question:
+What is Maximum Likelihood Estimation, and why is it used in training the Bayesian Network?
+
+ Maximum Likelihood Estimation (MLE) is a method used to estimate the parameters of a statistical model. 
+ In this context, MLE is used to estimate the conditional probability distributions of symptoms given the presence or absence of the flu. 
+ It is employed to maximize the likelihood of observing the given dataset under the assumed model.
+'''
+
+
+# ***NODES
 columns = ['Flu', 'Fever', 'Fatigue', 'Cough', 'Headache', 'SoreThroat', 'BodyAche', 'ChestPain', 'Chills', 'Nausea',
            'ShortnessOfBreath', 'LossOfTaste', 'LossOfSmell']
+
+# ***We have created an empty DataFrame to store the synthetic dataset
 data = pd.DataFrame(columns=columns)
 
+# https://stackoverflow.com/questions/43664994/is-there-a-difference-between-the-input-paramaters-of-numpy-random-choice-and-ra
+# https://stackoverflow.com/questions/73724150/problem-with-np-random-multinomial-and-size-option-in-python?rq=2
+# https://www.w3schools.com/jsref/jsref_random.asp
 for _ in range(num_samples):
     flu = np.random.choice([0, 1])
     fever_prob = 0.5 if flu == 1 else 0.2
@@ -32,7 +82,7 @@ for _ in range(num_samples):
     shortness_of_breath_prob = 0.6 if flu == 1 else 0.4
     loss_of_taste_prob = 0.5 if flu == 1 else 0.5
     loss_of_smell_prob = 0.4 if flu == 1 else 0.6
-
+# https://arxiv.org/abs/cs/9501101
     fever = np.random.choice([0, 1], p=[1 - fever_prob, fever_prob])
     fatigue = np.random.choice([0, 1], p=[1 - fatigue_prob, fatigue_prob])
     cough = np.random.choice([0, 1], p=[1 - cough_prob, cough_prob])
@@ -56,8 +106,10 @@ data.to_csv('synthetic_dataset_complex.csv', index=False)
 
 model.fit(data, estimator=MaximumLikelihoodEstimator)
 
+# ***We set up Variable Elimination for inference in the Bayesian Network
 inference = VariableElimination(model)
 
+# REFERENCE: https://flask.palletsprojects.com/en/2.1.x/quickstart/#rendering-templates
 @app.route('/research')
 def research():
     return render_template('research.html')
@@ -70,15 +122,17 @@ def index():
 @app.route('/diagnose', methods=['POST'])
 def diagnose():
     symptoms = request.form.getlist('symptoms')
-
     if not symptoms:
         return render_template('diagnosis.html', diagnosis="No illnesses selected", probability=None)
 
+# ***Perform inference to diagnose the likelihood of having the flu
     query_result = inference.query(variables=['Flu'], evidence=dict(zip(symptoms, [1]*len(symptoms))))
     probability = query_result.values[1]
 
     diagnosis = "Covid-19" if probability > 0.5 else "Influenza"
+# https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application
 
+# ***Render the diagnosis page with the result
     return render_template('diagnosis.html', diagnosis=diagnosis, probability=probability)
 
 
